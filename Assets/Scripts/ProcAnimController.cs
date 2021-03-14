9ccb568a99d90b35ccb400c5f79d72179911a11c
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2021-03-14 18:48:27 -0400

    Dotween overshoot only

--- Assets/Scripts/ProcAnimController.cs
+++ Assets/Scripts/ProcAnimController.cs
@@ -7,7 +7,8 @@ public class ProcAnimController : MonoBehaviour
 {
     // Start is called before the first frame update
     public LayerMask isGround;
-    public float stepThreshold = 1.3f;
+    public float stepThreshold =  0.8f;
+    public float stepOvershoot = 0.2f;
     public Vector3 downwardCastOffset = new Vector3(0f, 0f, 0.75f);
     public Transform targetIK;
     public Vector3 targetOffset = new Vector3(0f, 0.1f, 0f);
@@ -32,7 +33,7 @@ public class ProcAnimController : MonoBehaviour
         }
         float distanceToHit = Vector3.Distance(transform.position, hit.point);
         float testDistance = Vector3.Distance(castPoint.position, currTarget);
-        Debug.Log(testDistance);
+        // Debug.Log(testDistance);
         if (testDistance > stepThreshold) {
             updateHit();
             Debug.Log(testDistance);
@@ -48,6 +49,9 @@ public class ProcAnimController : MonoBehaviour
         Vector3 castStart = castPoint.position;
         if (Physics.Raycast(castStart, transform.TransformDirection(Vector3.down), out hit, Mathf.Infinity, isGround))
         {
+            Vector3 diff =  currTarget - hit.point;
+            Debug.Log(diff.normalized);
+            currTarget = hit.point;
             currTarget = hit.point;
             Debug.DrawRay(castStart, transform.TransformDirection(Vector3.down) * hit.distance, Color.yellow);
             Debug.Log("Did Hit");
@@ -60,7 +64,8 @@ public class ProcAnimController : MonoBehaviour
         }
     }
     void updateTargetIKTransform() {
-        targetIK.DOMove(currTarget + targetOffset, 0.2f).SetEase(Ease.InQuad);
+        targetIK.DOMove(currTarget + targetOffset, 0.1f).SetEase(Ease.InQuad, stepOvershoot);
+        targetIK.position = currTarget + targetOffset;
         Vector3 orient = Vector3.Cross(hit.normal, targetIK.forward);
         targetIK.up = -hit.normal;
     }
